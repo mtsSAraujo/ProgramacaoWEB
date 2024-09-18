effcd3ec2d4bbd17376277802fba938f693a8024
mtsSAraujo <dudtcyccj@gmail.com>
2024-09-17 22:09:30 -0300

    commit aula 17/09

--- Aula04/env/main.py
+++ Aula04/env/main.py
@@ -1,14 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route("/")
+@app.route("/", methods=["GET"])
 def imc():
     return render_template("imc.html")
 
 @app.route("/calcular_imc_post", methods = ['POST'])
 def calcular_imc_post():
-    return 'Hello world'
+    altura = float(request.form["txt_altura"])
+    peso = float(request.form["txt_peso"])
+    imc = peso / (altura ** 2)
+    return render_template("imc.html", imc=format(imc:.2f))
 
+"""
+@app.route("/", methods=['GET', 'POST'])
+def calcular_imc_post():
+    imc = None  # Variável para armazenar o valor do IMC
+    if request.method == 'POST':
+        altura = float(request.form["txt_altura"])
+        peso = float(request.form["txt_peso"])
+        imc = peso / (altura ** 2)  # Cálculo do IMC
+
+    # Renderiza o template com o valor do IMC (se disponível)
+    return render_template("imc.html", imc="{imc:}")
+"""
 
 app.run()
