e01568d477c7d06c224063cedd2521626851cc1b
mtsSAraujo <dudtcyccj@gmail.com>
2024-10-10 22:01:00 -0300

    new mySQL connector lib

--- AulaBancoDeDados/env/main.py
+++ AulaBancoDeDados/env/main.py
@@ -1,3 +1,17 @@
 from flask import Flask, render_template, request
+from mysql_connector import
 
 app = Flask(__name__)
+
+@app.route("/")
+def mainPage():
+    return render_template("cad_usuario.html")
+
+@app.route("/inserir_usuario", methods = ["POST"])
+def inserirUsuario():
+    nome = request.form["txt_nome"]
+    email = request.form["txt_email"]
+    senha = request.form["txt_senha"]
+    return nome, email, senha
+
+app.run()
