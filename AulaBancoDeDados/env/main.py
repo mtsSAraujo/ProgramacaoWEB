41879db620333d435f7242e9cb8ac2a62d2e03d6
mtsSAraujo <dudtcyccj@gmail.com>
2024-10-22 21:45:39 -0300

    feat new changes

--- AulaBancoDeDados/env/main.py
+++ AulaBancoDeDados/env/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request
 import mysql.connector
+from mysql import connector
 
 app = Flask(__name__)
 
@@ -12,6 +13,18 @@ def inserirUsuario():
     nome = request.form["txt_nome"]
     email = request.form["txt_email"]
     senha = request.form["txt_senha"]
-    db = mysql.connector.connect(host = "201.23.3.86", user = "usr_aluno", password = "E$tud@_m@1$", port = 5000, database = "aula_fatec")
+    db = mysql.connector.connect(host = "201.23.3.86",
+                                user = "usr_aluno",
+                                password = "E$tud@_m@1$",
+                                port = 5000,
+                                database = "aula_fatec"
+                            )
+    query = "INSERT INTO Mateus_tbusuario (nome, email, senha) VALUES ( %s, %s, %s)"
+    valores = (nome, email, senha)
+
+    meu_cursor = db.cursor()
+    meu_cursor.execute(query,valores)
+    db.commit()
+    return render_template("cad_usuario.html")
 
 app.run()
