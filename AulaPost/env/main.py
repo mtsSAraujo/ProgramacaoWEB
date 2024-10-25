7d1aa829e119eecea05ed68e720346ff80c4fe01
mtsSAraujo <dudtcyccj@gmail.com>
2024-10-24 21:49:33 -0300

    mudanças html

--- AulaPost/env/main.py
+++ AulaPost/env/main.py
@@ -1,85 +1,89 @@
 from flask import Flask, render_template, request, flash
 import mysql.connector
-from mysql.connector import Error
 
 app = Flask(__name__)
 
-@app.route("/")
-def mainPage():
+@app.route("/login")
+def loginPage():
+    return render_template("login.html")
+
+@app.route("/login", methods = ["POST"])
+def validarLogin():
+    usuario = request.form["txt_usuario"]
+    senha = request.form["txt_senha"]
+
+    db = mysql.connector.connect(
+        host="201.23.3.86",
+        user="usr_aluno",
+        password="E$tud@_m@1$",
+        port=5000,
+        database="aula_fatec"
+    )
+
+    query = "SELECT usuario, senha from Mateus_tbusuario where usuario = %s and senha = %s"
+    meu_cursor = db.cursor()
+    meu_cursor.execute(query, (usuario, senha))
+    if meu_cursor.fetchall():
+        return "Deu certo"
+    else:
+        return render_template("login.html", senhaErrada = "Usuário ou senha inválido")
+
+@app.route("/user")
+def userPage():
     return render_template("cad_usuario.html")
 
-@app.route("/inserir_usuario", methods = ["POST"])
+@app.route("/user", methods = ["POST"])
 def inserirUsuario():
-    try:
-        nome = request.form["txt_nome"]
-        email = request.form["txt_email"]
-        senha = request.form["txt_senha"]
+    usuario = request.form["txt_usuario"]
+    email = request.form["txt_email"]
+    senha = request.form["txt_senha"]
 
-        db = mysql.connector.connect(
-            host="201.23.3.86",
-            user="usr_aluno",
-            password="E$tud@_m@1$",
-            port=5000,
-            database="aula_fatec"
-        )
+    db = mysql.connector.connect(
+        host="201.23.3.86",
+        user="usr_aluno",
+        password="E$tud@_m@1$",
+        port=5000,
+        database="aula_fatec"
+    )
 
-        query = "INSERT INTO Mateus_tbusuario (nome, email, senha) VALUES (%s, %s, %s)"
-        valores = (nome, email, senha)
+    query = "INSERT INTO Mateus_tbusuario (usuario, email, senha) VALUES (%s, %s, %s)"
+    valores = (usuario, email, senha)
 
-        meu_cursor = db.cursor()
-        meu_cursor.execute(query, valores)
-        db.commit()
+    meu_cursor = db.cursor()
+    meu_cursor.execute(query, valores)
+    db.commit()
 
-        flash("Usuário inserido com sucesso!")
-        return render_template("cad_usuario.html")
+    return render_template("cad_usuario.html")
 
-    except Error as e:
-        print(f"Erro ao conectar ou executar query no banco de dados: {e}")
-        flash(f"Ocorreu um erro ao inserir o usuário: {e}")
-        return render_template("erro.html")
+@app.route("/client")
+def clientPage():
+    return render_template("cad_cliente.html")
 
-    finally:
-        if db.is_connected():
-            meu_cursor.close()
-            db.close()
-
-@app.route("/inserir_cliente", methods = ["POST"])
+@app.route("/client", methods = ["POST"])
 def inserirCliente():
-    try:
-        nome = request.form["txt_nome"]
-        cpf = request.form["txt_cpf"]
-        rg = request.form["txt_rg"]
-        endereco = request.form["txt_endereco"]
-        bairro = request.form["txt_bairro"]
-        cidade = request.form["txt_cidade"]
-        cep = request.form["txt_cep"]
+    nome = request.form["txt_nome"]
+    cpf = request.form["txt_cpf"]
+    rg = request.form["txt_rg"]
+    endereco = request.form["txt_endereco"]
+    bairro = request.form["txt_bairro"]
+    cidade = request.form["txt_cidade"]
+    cep = request.form["txt_cep"]
 
-        db = mysql.connector.connect(
-            host="201.23.3.86",
-            user="usr_aluno",
-            password="E$tud@_m@1$",
-            port=5000,
-            database="aula_fatec"
-        )
+    db = mysql.connector.connect(
+        host="201.23.3.86",
+        user="usr_aluno",
+        password="E$tud@_m@1$",
+        port=5000,
+        database="aula_fatec"
+    )
 
-        query = "INSERT INTO Mateus_tbcliente (nome, cpf, rg, endereco, bairro, cidade, cep) VALUES (%s, %s, %s, %s, %s, %s, %s)"
-        valores = (nome, cpf, rg, endereco, bairro, cidade, cep)
+    query = "INSERT INTO Mateus_tbcliente (nome, cpf, rg, endereco, bairro, cidade, cep) VALUES (%s, %s, %s, %s, %s, %s, %s)"
+    valores = (nome, cpf, rg, endereco, bairro, cidade, cep)
 
-        meu_cursor = db.cursor()
-        meu_cursor.execute(query, valores)
-        db.commit()
+    meu_cursor = db.cursor()
+    meu_cursor.execute(query, valores)
+    db.commit()
 
-        flash("Cliente inserido com sucesso!")
-        return render_template("cad_usuario.html")
-
-    except Error as e:
-        print(f"Erro ao conectar ou executar query no banco de dados: {e}")
-        flash(f"Ocorreu um erro ao inserir o usuário: {e}")
-        return render_template("erro.html")
-
-    finally:
-        if db.is_connected():
-            meu_cursor.close()
-            db.close()
+    return render_template("cad_cliente.html")
 
 app.run(debug = True)
